eafffedb7976f0ed39e4785c4e66f89cab6a2793
SobolevIvan <sobolev.is.rus@gmail.com>
2019-01-08 11:47:03 +0700

    First scene

---
+++ GeekBrains/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animation _animation;
+
+    void Start()
+    {
+        _animation = GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
